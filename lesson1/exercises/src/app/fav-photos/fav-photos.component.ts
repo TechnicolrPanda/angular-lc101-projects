556d081d9879c1b58d6b658bed8eb4ab79ee1d36
TechnicolrPanda <amelia.a.fitzpatrick@gmail.com>
2021-04-11 20:31:37 -0500

    finished first part of angular lesson 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Random Images';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'http://www.aywas.com/aywas/user/avatar/03/26/10/1d5382536b30fb4645a73723d57a88ed.png';
+  image3 = 'https://i.imgur.com/4Lz0LdI.png';
 
   constructor() { }
 
