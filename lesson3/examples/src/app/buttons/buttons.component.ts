5b21c9e25b781c57630f5a900f2c5ad6ee040a34
TechnicolrPanda <amelia.a.fitzpatrick@gmail.com>
2021-04-16 20:01:18 -0500

    finished examples

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -4,13 +4,22 @@ import { Component, OnInit } from '@angular/core';
    selector: 'buttons',
    templateUrl: './buttons.component.html',
    styleUrls: ['./buttons.component.css']
+   
 })
 export class ButtonsComponent implements OnInit {
    buttonHeading: string = "Buttons"
    inactive: boolean = false;
+   buttonState: boolean[] = [true, true, true];
 
    constructor() { }
 
    ngOnInit() { }
+
+   reactivateButtons() {
+      for (let i=0; i < this.buttonState.length; i++){
+         this.buttonState[i] = true;
+      }
+      return this.buttonState
+   }
 
 }
